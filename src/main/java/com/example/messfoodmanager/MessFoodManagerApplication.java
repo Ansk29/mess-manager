42767f6979f415ebd6d6c1cbc3c033e7c39848b5
Anshul Kakirde <anshul.kakirde@gmail.com>
2025-06-08 10:21:56 +0530

    initial commit

---
+++ src/main/java/com/example/messfoodmanager/MessFoodManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.messfoodmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessFoodManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessFoodManagerApplication.class, args);
+    }
+
+}
